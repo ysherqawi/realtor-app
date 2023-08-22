ac70015067c2da62d00d2631c21257173e19eecd
ysherqawi <ysherqawi@gmail.com>
2023-08-22 17:06:56 +0300

    validate the user email

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface SignupParams {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+}
 
 @Injectable()
 export class AuthService {
-  signup() {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async signup({ email }: SignupParams) {
+    const userExist = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (userExist) {
+      throw new ConflictException();
+    }
+
+    console.log(userExist);
+  }
 }
