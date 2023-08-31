35c7582797409fb0453ac18cc00bd67fec34ea7d
ysherqawi <ysherqawi@gmail.com>
2023-08-31 12:39:39 +0300

    create custom user interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { HomeModule } from './home/home.module';
+import { UserInterceptor } from './user/interceptors/user.interceptor';
 
 @Module({
   imports: [UserModule, PrismaModule, HomeModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: UserInterceptor },
+  ],
 })
 export class AppModule {}
